4bf7046e5e076825712e7a12a06b547f51120989
Matan Mizrahi <matmiz1@gmail.com>
2018-08-25 16:57:04 +0300

    some refactoring

--- gen.js
+++ gen.js
@@ -1,53 +1,10 @@
-const fs = require('file-system');
-const createComponents = require('./scripts/createComponents');
-const createActions = require('./scripts/createActions');
-const createReducers = require('./scripts/createReducers');
-const createIndexFiles = require('./scripts/createIndexFiles');
-const Input = require('prompt-input');
-
-const input = new Input({
-  name: 'projectName',
-  message: 'Please type a prefix to your project (use only characters, starting with capital one):'
-});
-
-const tryAgainInput = new Input({
-    name: 'tryAgain',
-    meesage: 'Please use only characters:'
-});
-
-const ask = () => {
-    input.ask(answer => {
-        const isStartingWithCapital = answer.match(/^[A-Z]/);
-        const isValid = answer.match(/[A-Z]/);
-        if(!isStartingWithCapital) {
-            input.message = "Components name should satrt with a capital letter"
-        }
-
-        if(!isValid) {
-            input.message = "Please use only characters:";
-            ask();
-        }
-        else {
-            createFoldersAndFiles(answer);
-        }
-    })
-};
-
-ask();
-
+const projectPrefixInput = require('./scripts/user-inputs/projectPrefix');
+const createFolders = require('./scripts/creators/createFolders');
+const createFiles = require('./scripts/creators/createFiles');
 
 function createFoldersAndFiles(prefix) {
-    //create folders
-    fs.mkdirSync('src');
-    fs.mkdir('src/Components');
-    fs.mkdir('src/Reducers');
-    fs.mkdir('src/Actions');
-
-    //create files
-    fs.writeFile('src/constants.js', `export const TEST_ACTION = 'testAction';`);
-    createComponents(prefix);
-    createActions(prefix);
-    createReducers(prefix);
-    createIndexFiles(prefix)
-    fs.writeFile('src/css/style.scss',`.main-container{}`);
+    createFolders();
+    createFiles(prefix);
 }
+
+projectPrefixInput(createFoldersAndFiles);
